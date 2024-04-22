a8f3c730917179cfd855d20d7780ba5bbbe6cc1b
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-04-22 10:12:52 -0400

    Adding an index file that exports everything.

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+//
+// Exports
+//
+
+export * from "./classes/chunks/Chunk.js";
+export * from "./classes/chunks/TextureChunk.js";
+
+export * from "./classes/BinaryReader.js";
+export * from "./classes/ChunkRegistry.js";
+
+export * from "./instances/default-chunk-registry.js";
+
+export * as ChunkLib from "./libs/chunk.js";
+export * as ChunkIdentifierLib from "./libs/chunk-identifier.js";
+export * as FileLib from "./libs/file.js";
+export * as FileSignatureLib from "./libs/file-signature.js";

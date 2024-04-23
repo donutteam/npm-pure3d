cc92dd600c89e4389f9285c9067a268ea275db1c
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-04-23 13:26:25 -0400

    Updating my test script.

--- src/test.ts
+++ src/test.ts
@@ -12,7 +12,7 @@ import * as Pure3D from "./index.js";
 // Test
 //
 
-const filePath = "E:\\Other\\The Simpsons Hit & Run\\Installs\\The Simpsons Hit & Run\\art\\chars\\a_amer_m.p3d";
+const filePath = "D:\\Desktop\\input.p3d";
 
 const fileData = await fs.promises.readFile(filePath);
 
@@ -21,13 +21,25 @@ const rootChunk = Pure3D.File.read(
 		arrayBuffer: fileData.buffer,
 	});
 
-const newFileBuffer = Pure3D.File.write(
+logChildren(rootChunk);
+
+const newFileData = Pure3D.File.write(
 	{
 		chunks: rootChunk.children,
-		littleEndian: false,
 	});
 
-await fs.promises.writeFile("D:\\Desktop\\test.p3d", Buffer.from(newFileBuffer),
+await fs.promises.writeFile("D:\\Desktop\\output.p3d", Buffer.from(newFileData));
+
+//
+// Functions
+//
+
+export function logChildren(chunk : Pure3D.Chunk, depth = 0)
+{
+	for (const child of chunk.children)
 	{
-		encoding: "binary",
-	});
+		console.log("\t".repeat(depth) + child.constructor.name);
+
+		logChildren(child, depth + 1);
+	}
+}

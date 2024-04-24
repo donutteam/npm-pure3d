17dcd2ec08663acb45067e37e53d2ece0344fe08
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-04-24 13:19:38 -0400

    Adding read-and-write test.

---
+++ src/tests/read-and-write.test.ts
@@ -0,0 +1,60 @@
+//
+// Imports
+//
+
+import fs from "node:fs";
+import path from "node:path";
+import test from "node:test";
+import url from "node:url";
+
+import * as Pure3D from "../index.js";
+
+//
+// Tests
+//
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const testFilesDir = path.join(__dirname, "..", "..", "test-files");
+
+await test.it("Read and write an existing Pure3D file",
+	async () =>
+	{
+		//
+		// Load P3D File
+		//
+
+		const originalFileName = "test.p3d";
+
+		const originalFilePath = path.join(testFilesDir, originalFileName);
+
+		const originalFileBuffer = await fs.promises.readFile(originalFilePath);
+
+		const rootChunk = Pure3D.File.read(
+			{
+				arrayBuffer: originalFileBuffer,
+			});
+
+		//
+		// Create New P3D File
+		//
+
+		const newFileBuffer = Pure3D.File.write(
+			{
+				addHistoryChunk: true,
+				chunks: rootChunk.children,
+			});
+
+		//
+		// Write P3D File
+		//
+
+		const outputDir = path.join(__dirname, "..", "..", "output");
+
+		await fs.promises.mkdir(outputDir,
+			{
+				recursive: true,
+			});
+
+		await fs.promises.writeFile(path.join(outputDir, "read-and-write.p3d"), Buffer.from(newFileBuffer));
+	});
